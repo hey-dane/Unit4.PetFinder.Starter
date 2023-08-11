d162b04f8a31e7dfc9e32b9c1166ddddf4c81c78
Dana <126463047+hey-dane@users.noreply.github.com>
2023-08-10 23:02:01 -0700

    module 30 base complete

--- index.js
+++ index.js
@@ -1,55 +1,72 @@
 // import the pets array from data.js
-const pets = require('./data');
+const pets = require("./data");
 
 // init express app
-const express = require('express');
+const express = require("express");
+const e = require("express");
 const app = express();
 
 const PORT = 8080;
 
 // GET - / - returns homepage
-app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
-
+app.get("/", (req, res) => {
+  // serve up the public folder as static index.html file
 });
 
 // hello world route
-app.get('/api', (req, res) => {
-    res.send('Hello World!');
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
 });
 
 // get all pets from the database
-app.get('/api/v1/pets', (req, res) => {
-    // send the pets array as a response
-
+// create end point route to the GET request to access all pets
+app.get("/api/v1/pets", (req, res) => {
+  // send the pets array as a response
+  res.json(pets);
 });
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
-    // get the owner from the request
+// create end point route to the GET request to access user's ownerName
+// create a parameter for ownerName
+// filter pets array by matches to ownerName
+// return pet details as a json response
 
+app.get("/api/v1/pets/owner", (req, res) => {
+  // get the owner from the request
+  const owner = req.query.owner;
 
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+  // find the pet in the pets array
+  const pet = pets.find((pet) => pet.owner === owner);
 
-    // send the pet as a response
-
+  // send the pet as a response]
+  if (pet) {
+    res.json(pet);
+  } else {
+    res.status(404).json({ error: "No matches found." });
+  }
 });
 
 // get pet by name
-app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
+// create end route to GET request to access user's by :name
+// create a paramter for name
+// find pets array by matches to name
+app.get("/api/v1/pets/:name", (req, res) => {
+  // get the name from the request
+  const name = req.params.name;
 
+  // find the pet in the pets array
+  const pet = pets.find((pet) => pet.name === name);
 
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
-
-    // send the pet as a response
-
+  // send the pet as a response
+  if (pet) {
+    res.json(pet);
+  } else {
+    res.status(404).json({ error: "No matches found." });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
+  console.log("Server is listening on port " + PORT);
 });
 
 module.exports = app;
